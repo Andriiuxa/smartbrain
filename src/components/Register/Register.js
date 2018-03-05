cb3dd6448d0cd38714b71f4187e186a9c27ca8c0
Andrej Gorovoj (gorovoja) <gorovoja@coventry.ac.uk>
2018-03-05 22:24:11 +0000

    api change

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://pure-dusk-43594.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
